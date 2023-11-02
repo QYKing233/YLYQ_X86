7dfc6dc8b2e05c7c9789f51b3568d06b69d2b57c
QYKing233 <61415390+QYKing233@users.noreply.github.com>
2023-11-02 21:31:42 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -8,7 +8,53 @@
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
-#
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+# Remove default luci-theme-argon & luci-app-argon-config
+rm -rf feeds/luci/themes/luci-theme-argon 
+rm -rf feeds/luci/applications/luci-app-argon-config
+
+
+# Modify golang_version for luci-app-Alist
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 21.x feeds/packages/lang/golang
+
+
+# Modify luci-theme-argon background images
+pushd package/community/luci-theme-argon/htdocs/luci-static/argon/img
+rm -rf ./bg1.jpg
+wget https://raw.githubusercontent.com/QYKing233/YLYQ_X86/main/data/bg1.jpg
+popd
+
+
+# Modify default IP & default hostname
+sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/YLYQ/g' package/base-files/files/bin/config_generate
+
+
+# Modify default shell to zsh
+sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
+
+
+# Modify kernel version
+# sed -i 's/5.15/5.10/g' target/linux/rockchip/Makefile
+
+
+# Modify banner
+pushd package/base-files/files/etc
+rm -rf ./banner
+wget https://raw.githubusercontent.com/QYKing233/YLYQ_X86/main/data/banner
+popd
+
+
+## Install oh-my-zsh
+mkdir -p files/root
+pushd files/root
+# Clone oh-my-zsh repository
+git clone https://github.com/robbyrussell/oh-my-zsh ./.oh-my-zsh
+# Install extra plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions ./.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ./.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-completions ./.oh-my-zsh/custom/plugins/zsh-completions
+wget https://raw.githubusercontent.com/QYKing233/YLYQ_X86/main/data/.zshrc
+popd
