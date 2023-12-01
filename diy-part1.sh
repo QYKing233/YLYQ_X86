b82566221223cb3346ad89c588cd9f55e41d75ae
QYKing233 <61415390+QYKing233@users.noreply.github.com>
2023-12-01 15:56:01 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,98 +1,93 @@
 #!/bin/bash
-#
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
-#### Clone community packages to package/community
 
+######################  添加自定义插件  #########################
+
+# 创建community目录
 mkdir -p package/community
+
+# 把community目录置为当前
 pushd package/community
 
 
-# Add luci-app-alist
+# 添加 luci-app-alist
 git clone https://github.com/sbwml/luci-app-alist
 
 
-# Add luci-app-passwall
+# 添加 luci-app-passwall
 svn export https://github.com/xiaorouji/openwrt-passwall/trunk/luci-app-passwall
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages.git
 
 
-# Add luci-app-ssr-plus
+# 添加 luci-app-ssr-plus
 git clone -b main --depth=1 https://github.com/fw876/helloworld.git
 
 
-# Add luci-app-vssr
+# 添加 luci-app-vssr
 svn export https://github.com/ysx88/openwrt-packages/trunk/luci-app-vssr
 svn export https://github.com/ysx88/openwrt-packages/trunk/lua-maxminddb
 
 
-# Add OpenClash
+# 添加 OpenClash
 svn export https://github.com/vernesong/OpenClash/trunk/luci-app-openclash
 
 
-# Add luci-app-onliner (need luci-app-nlbwmon)
+# 添加 luci-app-onliner (依赖 luci-app-nlbwmon)
 git clone --depth=1 https://github.com/rufengsuixing/luci-app-onliner.git
 
 
-# Add luci-app-poweroff
+# 添加 luci-app-poweroff
 git clone --depth=1 https://github.com/esirplayground/luci-app-poweroff.git
 
 
-# Add luci-app-adguardhome
+# 添加 luci-app-adguardhome
 svn export https://github.com/Boos4721/OpenWrt-Packages/trunk/luci-app-adguardhome
 
 
-# Add luci-app-argon-config & luci-app-argon 
+# 添加 luci-app-argon-config & luci-app-argon 
 git clone -b 18.06 https://github.com/jerrykuku/luci-app-argon-config.git
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
 
 
-# Add open-app-filter
+# 添加 open-app-filter
 git clone --depth=1 https://github.com/destan19/OpenAppFilter.git
 
 
-# Add luci-app-syncthing
+# 添加 luci-app-syncthing
 svn export https://github.com/immortalwrt/luci/branches/openwrt-18.06/applications/luci-app-syncthing
 
 
-# Add luci-app-filebrowser
+# 添加 luci-app-filebrowser
 git clone -b 18.06 https://github.com/xiaozhuai/luci-app-filebrowser.git
 
 
-# Add luci-app-fileassistant
+# 添加 luci-app-fileassistant
 svn export https://github.com/kenzok8/small-package/trunk/luci-app-fileassistant
 
 
-# Add luci-app-beardropper
+# 添加 luci-app-beardropper
 svn export https://github.com/kenzok8/small-package/trunk/luci-app-beardropper
 
 
-# Add luci-app-irqbalance
+# 添加 luci-app-irqbalance
 svn export https://github.com/QiuSimons/OpenWrt-Add/trunk/luci-app-irqbalance
 
 
-# Add luci-app-ssr-mudb-server
+# 添加 luci-app-ssr-mudb-server
 svn export https://github.com/Lienol/openwrt-package/trunk/luci-app-ssr-mudb-server
 
 
-# Add luci-app-ddnsgo
+# 添加 luci-app-ddnsgo
 git clone --depth=1 https://github.com/sirpdboy/luci-app-ddns-go
 
 
-# Add luci-app-gost
+# 添加 luci-app-gost
 svn export https://github.com/kenzok8/small-package/trunk/luci-app-gost
 svn export https://github.com/kenzok8/small-package/trunk/gost
 
 
-# Add luci-app-unblockneteasemusic
+# 添加 luci-app-unblockneteasemusic
 svn export https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic/branches/master luci-app-unblockneteasemusic
 
 
-### exit package/community
 popd
+########################   添加完成  ###########################
